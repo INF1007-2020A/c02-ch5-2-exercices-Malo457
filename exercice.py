14a6c70cc6b90fdc3986fbf142a2909d4d3744e4
Malo457 <malostamourbilodeau@gmail.com>
2020-09-24 20:10:16 -0400

    Exercices version 1

--- exercice.py
+++ exercice.py
@@ -4,19 +4,44 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	num = 0
+
+	for car in text:
+		if car.isalnum():
+			num += 1
+
+	return num
 
 def get_word_length_histogram(text):
-	return [0]
+	histogramme = [0]
+	for mot in text.split():
+		length = get_num_letters(mot)
+
+		if length >= len(histogramme):
+			histogramme += [0]*(length - len(histogramme) + 1)
+		histogramme[length] += int(length != 0)
+
+	return histogramme
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
-
-	return ""
+	numligne = 0
+	affichage = ''
+	alignment = len(str(len(histogram) - 1))
+	for elem in histogram:
+		if numligne > 0:
+			affichage += f'{numligne : >{alignment}} '+ (ROW_CHAR * elem) + '\n'
+		numligne += 1
+	#'\n'.join([f'{numligne : >{alignment}} {ROW_CHAR * elem}' for numligne, elem in enumerate(histogram) if numligne != 0])
+	return affichage
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
+	hauteur = max(histogram)
+	for num in histogram:
+		if num == hauteur:
+
 
 	return ""
 
